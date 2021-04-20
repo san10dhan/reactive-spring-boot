ed6df033d04e32e51baccaabed0a8a36076aa78e
kiki <kiki@mailsoul.com>
2021-04-20 12:45:17 +0530

    reactive backend version 1

---
+++ src/main/java/com/app/sprangular/ReactiveAppFullStackApplication.java
@@ -0,0 +1,13 @@
+package com.app.sprangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveAppFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveAppFullStackApplication.class, args);
+	}
+
+}
